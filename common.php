6b8965327bd773f48a2aecd0490414b84b991df5
Cousjava <outingbrains-websitesgeneric@yahoo.co.uk>
2010-12-23 12:30:56 +0000

    Included a couple of general bugfixes, and made it so that the server will work with PHP 6.

--- common.php
+++ common.php
@@ -380,4 +380,21 @@ if ($session['user']['loggedin']) {
 	modulehook("everyhit-loggedin");
 }
 
+// Fix for removed Session functions in PHP 6
+function fix_session_register(){ 
+    function session_register(){ 
+        $args = func_get_args(); 
+        foreach ($args as $key){ 
+            $_SESSION[$key]=$GLOBALS[$key]; 
+        } 
+    } 
+    function session_is_registered($key){ 
+        return isset($_SESSION[$key]); 
+    } 
+    function session_unregister($key){ 
+        unset($_SESSION[$key]); 
+    } 
+} 
+if (!function_exists('session_register')) fix_session_register(); 
+
 ?>
